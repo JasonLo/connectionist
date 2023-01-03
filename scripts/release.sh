3792d2540fb97afb234114e3d7ab3592bae56e23
Jason Lo <lcmjlo@gmail.com>
2023-01-03 22:50:53 +0000

    update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,10 +1,19 @@
+#!/bin/sh
+
+# Get release version
+RELEASE_VERSION=`grep version pyproject.toml`
+
 rm -r dist
 python3 -m build
-# Release to testpypi
-# python3 -m twine upload --repository testpypi dist/*
 
 # Release to pypi
 python3 -m twine upload dist/*
 
-# account: __token__
-# password: get token from pypi.org
+# Tag in Git
+git tag $RELEASE_VERSION -m "Release $RELEASE_VERSION"
+git push --tags
+
+# Remember to add the following to ~/.pypirc
+# [pypi]
+# username = __token__
+# password = <token from pypi.org>
