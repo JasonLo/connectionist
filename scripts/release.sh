05c0e7cda850799e4a7adcf0b79d0a1150aec061
Jason Lo <lcmjlo@gmail.com>
2023-01-03 22:57:40 +0000

    update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Get release version
-RELEASE_VERSION=`grep version pyproject.toml`
+RELEASE_VERSION=`grep version pyproject.toml | cut -d '"' -f 2`
 
 rm -r dist
 python3 -m build
@@ -9,7 +9,7 @@ python3 -m build
 # Release to pypi
 python3 -m twine upload dist/*
 
-# Tag in Git
+# Tag in Git version
 git tag $RELEASE_VERSION -m "Release $RELEASE_VERSION"
 git push --tags
 
